1d9f2f249df4bcbd10732c4e7d1ba7b8e2757820
lman88 <line@manens.org>
2021-03-20 15:59:04 +0100

    essai map start exit

--- cuisine.tsx
+++ cuisine.tsx
@@ -1,4 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="cuisine" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="../../Downloads/cuisine.png" width="512" height="512"/>
+ <tile id="132">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="133">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
